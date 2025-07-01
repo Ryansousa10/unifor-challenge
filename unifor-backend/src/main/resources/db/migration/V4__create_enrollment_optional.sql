19cb101cafbe7e9ed7f1da567f0df9ee95bdb12a
Ryan Sousa <Ryan.sousa4000@gmail.com>
2025-07-01 18:33:22 -0300

    fix(db): renomeia tabela "user" para users e atualiza referências

--- unifor-backend/src/main/resources/db/migration/V4__create_enrollment_optional.sql
+++ unifor-backend/src/main/resources/db/migration/V4__create_enrollment_optional.sql
@@ -1,6 +1,6 @@
 CREATE TABLE enrollment (
                             id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                            student_id    UUID NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
+                            student_id    UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
                             curriculum_id UUID NOT NULL REFERENCES curriculum(id) ON DELETE CASCADE,
                             enrolled_at   TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
